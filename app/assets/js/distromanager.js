5457bf21278c258e8eb9306f624b0a8a012c7a3a
SIPREV <kapi.joker2003@gmail.com>
2025-03-28 06:32:49 +0100

    sdgd

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://siprev.github.io/Siprev-Launcher-/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://siprev.byethost22.com/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
